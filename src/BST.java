dce95e22768881005e6af90a5c20a9b2662f76d0
guzal <220930@astanait.edu.kz>
2023-05-18 18:40:43 +0600

    STEP 1: Creating of BTS class and adding inner class.

--- src/BST.java
+++ src/BST.java
@@ -1,2 +1,10 @@
-public class BST {
+public class BST<K extends Comparable<K>, V> {
+    private Node root;
+
+    // Inner class representing a node in the binary search tree
+    private class Node {
+        private K key;
+        private V value;
+        private Node left, right;
+    }
 }
